5d617a3ba93e03dfb6b5728f5f672519877d60c0
deepakchanana <deepakdeep1992@gmail.com>
2020-02-16 11:56:08 -0800

    I am pushing my cide

---
+++ Project_1/src/pkg1/Class1.java
@@ -0,0 +1,25 @@
+package pkg1;
+
+import java.util.Scanner;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver.Options;
+import org.openqa.selenium.WebDriver.Window;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Class1
+{
+  public static void main(String[] args) 
+  {
+	
+	System.setProperty("webdriver.chrome.driver", "C:/Users/Dell/Desktop/chromedriver.exe");
+	ChromeDriver driver=new ChromeDriver();
+	driver.get("https://www.facebook.com");
+    driver.manage().window().maximize();  // method chaining
+   
+    WebElement element=driver.findElement(By.cssSelector("input.inputtext.login_form_input_box"));
+    element.sendKeys("lastnamedeepak");
+  }
+	
+}
